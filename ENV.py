9030823039c5c70b64609148c91252800fd5408e
ba1ab <iab3208@outlook.com>
2025-09-15 16:54:51 +0800

    main

--- ENV.py
+++ ENV.py
@@ -5,23 +5,29 @@ import numpy as np
 
 class uavENV:
 
-    def __init__(self, n_agents):
-        self.n_agents = n_agents
+    def __init__(self, n_uavs, n_ues):
+        self.n_uavs = n_uavs
+        self.n_ues = n_ues
+
         self.height = self.width = self.length =100 # m
 
-        self.uav_location = np.array([[0, 0] for _ in range(self.n_agents)])
-        self.uav_velocity = 5 # m/s
+        self.uav_velocity = 5.0 # m/s
+        self.t_fly = 1.0 
+        self.t_com = 7.0
+        self.delta_t = self.t_fly + self.t_com
+
+        self.uav_locations = np.zeros((self.n_uavs, 2))
+        self.uav_battery = np.zeros(self.n_uavs)
+
+        self.ue_locations = np.zeros((self.n_ues, 2))
+        self.ue_tasks = np.zeros(self.n_ues, dtype=np.float64)
+
+        self.step_coounter = 0
+
+
+    def reset(self):
         
-        self.uav_energy = np.zeros(self.n_agents)
-        self.uav_size = np.zeros(self.n_agents)
 
-
-
-        self.n_ue = 4
-        self.ue_location = np.array([[0, 0] for _ in range(self.n_ue)])
-        self.ue_velocity = 2 # m/s
-
-        for n in range(self.n_agents):
-            
+        
         
 
